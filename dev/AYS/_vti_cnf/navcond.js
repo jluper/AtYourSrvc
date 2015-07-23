91d0f979dfc1db7d5e7fcc9f77698191de90095a
jack luper <jluper@triad.rr.com>
2015-07-23 14:00:51 -0400

    initial commit

---
+++ dev/AYS/_vti_cnf/navcond.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Mar 2002 21:16:42 -0000
+vti_extenderversion:SR|5.0.2.2634
+vti_backlinkinfo:VX|AYS/contact_us.htm AYS/register_confirmation.htm AYS/Notice.htm AYS/index.html AYS/register_with_us.htm AYS/services.htm AYS/about_us.htm AYS/in_the_news.htm
